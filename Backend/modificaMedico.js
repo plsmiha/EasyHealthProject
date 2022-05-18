5c203bcb3c163f9ba611c6f6574986630b8bbee1
ThomasAndreatta <thomas.andreatta2000@gmail.com>
2022-05-18 22:42:15 +0200

    update password e user

--- Backend/modificaMedico.js
+++ Backend/modificaMedico.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const docs = require('./models/doc');
 const user = require('./models/user');
+const crypto = require('crypto');
 router.get('', async function(req, res) {
     var _id = getId();
     let data = await docs.findById(_id);
@@ -24,6 +25,14 @@ function getUser()//qui dentro ricevaero dal jwt l'id user
 {
   return '6283a725e782702e1e1e21fe';
 }
+
+function creaPassword(pass){
+  var hash = crypto.createHash('sha512');
+  data = hash.update(pass, 'utf-8');
+  gen_hash= data.digest('hex');
+  return gen_hash;
+}
+
 router.post('', async function(req, res) {
         console.log('ecco una post');
         var email=req.body.email;
@@ -31,46 +40,49 @@ router.post('', async function(req, res) {
         var password=req.body.password;
         var title=req.body.title;
         var _id=getId();
-        var _user=getUser();
-
-        if(password.length == 0){
 
 
+        docs.findByIdAndUpdate({_id},{"email": email,"address":address,"title":title}, function(err, result){
+            console.log('sono nella update Medic');
+            if(err){
+                  console.log(err);
+                  res.status(406).json({success: 'false', reason: 'esploso il db', error: '3'});
+                  return;
+        }
+        else{
+                  console.log('yuppi');
 
-          docs.findByIdAndUpdate({_id},{"email": email,"address":address,"title":title}, function(err, result){
-              console.log('sono nella update Medic');
+        }})
+
+        var _id=getUser();
+
+        if(password.length > 0){
+          user.findByIdAndUpdate({_id},{"email": email,"password":creaPassword(password)}, function(err, result){
+              console.log('sono nella update User');
+              console.log(creaPassword(password));
               if(err){
                     console.log(err);
                     res.status(406).json({success: 'false', reason: 'esploso il db', error: '3'});
                     return;
           }
-          else{
-                    console.log('yuppi');
-
-          }})
-
-          var _id=getUser();
-          user.findByIdAndUpdate({_id},{"email": email,}, function(err, result){
+          })
+          return;
+        }
+        else{
+          user.findByIdAndUpdate({_id},{"email": email}, function(err, result){
               console.log('sono nella update User');
               if(err){
                     console.log(err);
                     res.status(406).json({success: 'false', reason: 'esploso il db', error: '3'});
                     return;
           }
-          else{
-                    console.log('yuppi');
-                    res.status(200).json({success: 'true',comment:'eheh'});
-                    return;
-          }})
+          })
           return;
         }
-        else{
-            console.log('diversa  di 0');
-            res.status(200).json({success: 'Modificati i dati ma non password',comment:'eheh'});
-            return;
-        }
 
+        console.log('yuppi');
         res.status(200).json({success: 'true',comment:'eheh'});
+        return;
         return;
 
 });
