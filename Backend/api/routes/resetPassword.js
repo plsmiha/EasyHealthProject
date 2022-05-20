212ec3793f87df965648503bfed858756653ff16
mihaela <mihaela.mereuta@studenti.unitn.it>
2022-05-20 11:14:33 +0200

    recupero campo email

--- Backend/api/routes/resetPassword.js
+++ Backend/api/routes/resetPassword.js
@@ -28,7 +28,8 @@ function generateString(length) {
 }
 
 
-
+//quando riceve una post fa questo
+//tramite browser faccio delle get
 router.post('', async function(req, res) {
 
    //RECUPERO IN QUALCHE MODO LA MAIL
@@ -39,18 +40,31 @@ router.post('', async function(req, res) {
    //la mando in chiaro al paziente 
     transporter.sendMail({
         from: '"EasyHealth+" <easy.health.app.info@gmail.com>',
-        to: req.body.email,                                                                   //EMAIL CHE DEVO ANCORA RECUPERARE
+        to: req.body.email,                                                                                           //EMAIL CHE DEVO ANCORA RECUPERARE
         subject: "",
-        text: ""
+        text: "use this temporary password to login and then change it woithin your profile" + passTemp,
     }).then().catch(console.error);
 
  
-
     //la hasho per salvarla nel db
     var hash = crypto.createHash('sha512');
     data = hash.update(passTemp, 'utf-8');
     gen_hash= data.digest('hex');
 
+    //recupero l'id della persona che ha quella mail 
+    //var utente= await User.find().where('email',req.body.email);                                                             //DA METTERE QUA LA EMAIL 
+    //var _id = Object.keys(user_check)
+    
+    //update user nel db cercandolo in base all'email
+    User.findOneAndUpdate(
+        {email: req.body.email},                                                                                                    //DA METTERE EMAIL
+        {"password":gen_hash}, 
+        function(err, result){ // For nearly all mongoose queries callback(err, results)
+            if(err){
+                res.status(406).json({success: 'false', reason: 'Errore update PSW temp ', error: '1'});
+                return;
+            }
+        })
 
 
     res.status(200).json({success: 'true', comment:'password temporanea modificata'});  
