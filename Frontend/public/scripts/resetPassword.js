7e07f32602c3a54f05506b727d1059958136f0d8
Thomas Andreatta <Thomas.Andreatta@zucchetti.it>
2022-05-20 11:11:49 +0200

    front end reset password

--- Frontend/public/scripts/resetPassword.js
+++ Frontend/public/scripts/resetPassword.js
@@ -1,10 +1,31 @@
-function checkpass() {
-  if (document.getElementById('pass1').value ==
-    document.getElementById('pass2').value) {
-    document.getElementById('pass2').style.background = 'lightgreen';
-    document.getElementById('l_notmatch').innerHTML = '';
-  } else {
-    document.getElementById('pass2').style.background = "#ff7a89";
-    document.getElementById('l_notmatch').innerHTML = 'Password not matching';
-  }
-}
+
+function startReset(){
+  var email=document.getElementById("Email").value;
+  fetch('../api/v1/resetPassword', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify( { email: email} ),
+  })
+  .then((resp) => resp.json())
+  .then(function(data) {
+      if(data.success=="true")
+      {
+          console.log('buon fine')
+          window.location.href = "index.html";
+      }
+      else
+      {
+          if(data.error=="2")
+          {
+            console.log('error 2')
+          }
+          else if(data.error=="3")
+          {
+            console.log('error 3 in caso miha implementi qualche error magico')
+          }
+      }
+  })
+  .catch( error => console.error(error) );
+
+
+};
