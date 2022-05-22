85fc2b9ee3ad6d847effdfc39a1a26382b7c06dd
JvJ00 <manuel.righele@studenti.unitn.it>
2022-05-22 21:32:35 +0200

    risoluzione primo bug email verificata

--- Backend/api/routes/login.js
+++ Backend/api/routes/login.js
@@ -20,14 +20,18 @@ router.post('', async function(req, res) {
         data = hash.update(req.body.password, 'utf-8');
         gen_hash= data.digest('hex');
 
-        if (gen_hash == user.password) {
+        if (user.verified && gen_hash == user.password) {
             console.log("utente loggato " + process.env.JWT_KEY)
 
             const token = jwt.sign({ id: user._id, role: user.type },  process.env.JWT_KEY);
             res.cookie("access_token", token, { httpOnly: true, secure: true}).status(200).json( { redirectTo: user.type });
 
-        } else {
-            console.log("tentativo di log errato")
+        } else if(!user.verified) {
+            console.log("utente non verificato");
+            res.status(403).json({ error: 'Unauthorized' });
+        }        
+        else{
+            console.log("tentativo di log errato");
             res.status(403).json({ error: 'Unauthorized' });
         }
     }
