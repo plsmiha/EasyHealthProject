56ed278e1bef831c87a88596ccb26a0387d0b2b2
JvJ00 <manuel.righele@studenti.unitn.it>
2022-05-31 09:54:34 +0200

    Sistemato modello AO

--- Backend/models/AO.js
+++ Backend/models/AO.js
@@ -3,11 +3,8 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model
 module.exports = mongoose.model('AOS', new Schema({
-	id_user: String,
-	email: String,
+	  id_user: String,
+	  email: String,
     name: String,
-    surname: String,
-    address: String,
-		CF: String,
-    title: String,
+    surname: String
 }));
