7e4aebd20768c299203198b8dd4e23c5dada4488
plsmiha <104876054+plsmiha@users.noreply.github.com>
2022-06-02 01:50:49 +0200

    Update doc.js

--- Backend/models/doc.js
+++ Backend/models/doc.js
@@ -3,11 +3,11 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model
 module.exports = mongoose.model('docs', new Schema({
-	id_user: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	id_user: { type: Schema.Types.ObjectId, ref: 'User' },
 	email: String,
     name: String,
     surname: String,
     address: String,
 		CF: String,
-    title: [{ type: Schema.Types.ObjectId, ref: 'aree' }],
+    title: { type: Schema.Types.ObjectId, ref: 'aree' },
 }));
