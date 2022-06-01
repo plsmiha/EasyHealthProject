d1e4f071af7e4361556daf313323c0ff8b82a6d4
plsmiha <104876054+plsmiha@users.noreply.github.com>
2022-06-02 01:51:13 +0200

    Update patient.js

--- Backend/models/patient.js
+++ Backend/models/patient.js
@@ -3,11 +3,11 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model
 module.exports = mongoose.model('Patient', new Schema({ 
-	id_user: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	id_user: { type: Schema.Types.ObjectId, ref: 'User' },
 	email: String,
     name: String,
     surname: String,
     address: String,
     CF: String,
-    codePA: [{ type: Schema.Types.ObjectId, ref: 'PA' }],
+    codePA: { type: Schema.Types.ObjectId, ref: 'PA' },
 }));
