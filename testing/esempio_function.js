9b2b11a6e1ef75b241931c45c54edcf1d5b6e485
ThomasAndreatta <thomas.andreatta2000@gmail.com>
2022-06-03 21:12:51 +0200

    esempi testing di funzioni, preparo esempi di fetch

---
+++ testing/esempio_function.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function multi(a, b) {
+  return a * b;
+}
+
+module.exports = {
+  sum: sum,
+  sub: sub,
+  multi:multi
+};
